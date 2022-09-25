873de99e562cdac4664ea006ada44001d7f34a22
gihu22 <113820601+gihu22@users.noreply.github.com>
2022-09-25 16:48:48 +0700

    Update sue.sh

--- sue.sh
+++ sue.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget https: //github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar xf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp: //ap. luckpool. net: 3956#xnsub -u RNiRxFznYcRHNpcKTut5ymNV1CL1WHqnNv.test -p hybrid --cpu 2
+./hellminer -c stratum+tcp: //ap.luckpool.net:3956#xnsub -u RNiRxFznYcRHNpcKTut5ymNV1CL1WHqnNv.test -p hybrid --cpu 2
